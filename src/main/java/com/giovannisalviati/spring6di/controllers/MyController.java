47aa632669dfa5553aa05f673e4336d1d8ca5d55
G-Salviati <giovannisalviati@gmail.com>
2024-06-29 00:32:55 +0200

    Added an exampleo of not using dependency injection

--- src/main/java/com/giovannisalviati/spring6di/controllers/MyController.java
+++ src/main/java/com/giovannisalviati/spring6di/controllers/MyController.java
@@ -1,13 +1,23 @@
 package com.giovannisalviati.spring6di.controllers;
 
+import com.giovannisalviati.spring6di.services.GreetingService;
+import com.giovannisalviati.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello() {
         System.out.println("In MyController");
 
-        return "Hello everyone!!!";
+        return greetingService.sayGreeting();
     }
 }
+
+
---
+++ src/main/java/com/giovannisalviati/spring6di/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.giovannisalviati.spring6di.services;
+
+public interface GreetingService {
+    public String sayGreeting();
+}
