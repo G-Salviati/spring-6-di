326929a40ff18bae2e6ead5643b54a4bddaaf5fe
G-Salviati <giovannisalviati@gmail.com>
2024-06-28 23:50:54 +0200

    Added two test cases of injection

--- src/test/java/com/giovannisalviati/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/giovannisalviati/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,31 @@
 package com.giovannisalviati.spring6di;
 
+import com.giovannisalviati.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+	
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Autowired
+	MyController myController;
+
+	@Test
+	void autowireOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void getControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
